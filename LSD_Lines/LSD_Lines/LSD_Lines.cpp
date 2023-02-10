e34bc184155a6fd29aaad4879e0f3e3753d4023a
Ahalikai <974260393@qq.com>
2023-02-10 16:49:13 +0800

    first commit

---
+++ LSD_Lines/LSD_Lines/LSD_Lines.cpp
@@ -0,0 +1,18 @@
+﻿#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+
+{
+	Mat img = imread("E:\\证件照\\头像.jpg");
+	namedWindow("测试");
+	imshow("测试", img);
+	waitKey(0);
+	return(0);
+
+}
