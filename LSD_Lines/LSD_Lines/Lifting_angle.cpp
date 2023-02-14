dce44f5ab8548e2c2d4226a960a9a80ab4fdc0ec
Ahalikai <974260393@qq.com>
2023-02-14 16:08:25 +0800

    2023_02_14 吊具位姿角度检测

--- LSD_Lines/LSD_Lines/Lifting_angle.cpp
+++ LSD_Lines/LSD_Lines/Lifting_angle.cpp
@@ -7,18 +7,36 @@
 #include <io.h>
 #include <string>
 #include <algorithm>
+#include <fstream>
+
 
 using namespace cv;
 using namespace std;
 
+//全局参数变量
+ofstream outFile;
+int line_minX = 10; //最短直线，为当前图像宽度的1/10
+float angle_max = 5.0; //直线筛选，不超过angle_max
+float line_where = 2.0; //直线筛选，在图像的上半部分，Eq：2.0为一半，3.0为三分之一
 
-void isDirExist(const char* dir); //判断文件夹是否存在，不存在则创建
+
+//判断文件夹是否存在，不存在则创建
+void isDirExist(const char* dir); 
+
+//吊具位姿检测主函数
+//image_dir：输入图像文件路径，图像文件名格式为 '%05d' 000XX.jpg。补零后，可按文件名顺序遍历图像
+//save_dir： 结果保存文件路径，输出result.txt。文件夹或txt不存在，可自动生成。
 void LSD_angle(const char* image_dir, const char* save_dir);
-// 图像文件名格式 '%05d' 000XX.jpg。补零后，可按文件名顺序遍历图像
-void LSD_img(const String);
-float cal_angle(float x1, float y1, float x2, float y2); //计算角度
+
+//单张图像的吊具位姿检测
+void LSD_img(Mat image);
+
+//单根直线的角度计算
+float cal_angle(float x1, float y1, float x2, float y2); 
+
 
 int main() {
+
 	const char* image_dir = "D:/pycharm/yolov7-main/Task_img/Test/box1/*.jpg";
 	const char* save_dir = "D:/pycharm/yolov7-main/Task_img/Test/result_2/";
 
@@ -37,6 +55,7 @@ void isDirExist(const char* dir) {
 		cout << "文件夹已经存在" << endl;
 }
 
+//单根直线的角度计算
 float cal_angle(float x1, float y1, float x2, float y2){
 	if (abs(x2 - x1) < 0.001) return 90.0;
 	else if (abs(y2 - y1) < 0.001) return 0.0;
@@ -47,8 +66,9 @@ float cal_angle(float x1, float y1, float x2, float y2){
 	}
 }
 
-void LSD_img(const String img_str) {
-	Mat image = imread(img_str);
+
+void LSD_img(Mat image) {
+	//Mat image = imread(img_str);
 	Mat grayImage;
 	cvtColor(image, grayImage, COLOR_BGR2GRAY);
 	// Create and LSD detector with standard or no refinement.
@@ -58,7 +78,7 @@ void LSD_img(const String img_str) {
 #else
 	Ptr<LineSegmentDetector> ls = createLineSegmentDetector(LSD_REFINE_NONE);
 #endif
-	vector<Vec4f> lines_std, lines_new;
+	vector<Vec4f> lines_std;
 	// Detect the lines
 	ls->detect(grayImage, lines_std);//这里把检测到的直线线段都存入了lines_std中，4个float的值，分别为起止点的坐标
 	/*
@@ -69,34 +89,60 @@ void LSD_img(const String img_str) {
 	}
 	*/  //检测点的坐标输出
 	int lines_num = lines_std.size();
+
+	float x_size = image.cols / line_minX; //最短长度
+	float angle_result = 0.0;
+	int angle_num = 0;
+
 	for (int i = 0; i < lines_num; i++) {
 		float angle = cal_angle(lines_std[i][0], lines_std[i][1]
 			, lines_std[i][2], lines_std[i][3]);
 
-		if(abs(angle) < 5)
-		cout << "angle: " << angle << endl;
+		float lines_x = abs(lines_std[i][2] - lines_std[i][0]); //直线x长度
+		if (abs(angle) < angle_max && lines_x > x_size && lines_std[i][1] < (image.rows / line_where)) {
+			//cout << "angle: " << angle << endl;
+			angle_num++;
+			angle_result += angle;
+		}
+		else {
+			lines_std[i][0] = lines_std[i][1] = lines_std[i][2] = lines_std[i][3] = 0;
+		}
 	}
 
+	if (angle_num > 0) {
+		angle_result /= angle_num;
+		outFile << angle_result << endl;
+		//cout << img_str << " ";
+		cout << angle_result << endl;
+	}
+	
 	// Show found lines
 	Mat drawnLines(image);
 	ls->drawSegments(drawnLines, lines_std);
 	imshow("Standard refinement", drawnLines);
-	waitKey(0);
+	waitKey(1);
 }
 
 void LSD_angle(const char* image_dir, const char* save_dir) {
 	isDirExist(save_dir);
+	
+	String save_txt = save_dir;
+	save_txt = save_txt + "result1.txt";
+	cout << "save_txt:  " << save_txt << endl;
+	outFile.open(save_txt);
 	Mat temp;
 
 	vector<cv::String> fn;
 	glob(image_dir, fn, false);
 
 	int img_num = fn.size();
-	for (int i = 0; i < 1; i++) {
-		cout << fn[i] << endl;
-		LSD_img(fn[i]);
+	for (int i = 0; i < img_num; i++) {
+		//cout << fn[i] << endl;
+		Mat image = imread(fn[i]);
+		LSD_img(image);
 	}
 		
-	//LSD_img("D:/pycharm/yolov7-main/Task_img/Test/box/237.jpg");
+	outFile.close();//将txt关闭
+	cout << "Ahalk：吊具位姿角度已检测完毕，数据已存入 -> " << save_txt << endl;
 	return;
 }
